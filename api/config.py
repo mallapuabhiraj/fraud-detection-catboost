0f55a3526a78e2219ee38131ea92d804bd28b5e6
Abhi Raju Mallapu <49338480+mallapuabhiraj@users.noreply.github.com>
2026-03-06 18:15:50 +0530

    Update config.py

--- api/config.py
+++ api/config.py
@@ -1,4 +1,4 @@
 MODEL_PATH = 'models/cb_model.cbm.
 PIPELINE_PATH = 'models/preprocess_pipe.joblib'
 FRAUD_THRESHOLD = 0.862
-CATEGORICAL_COLS = ['age_group', category', 'state', 'job']
+CAT_COLS = ['age_group', category', 'state', 'job']
