1611a955df9e06acae17292d2f1b96b80d2f7847
Abhi Raju Mallapu <49338480+mallapuabhiraj@users.noreply.github.com>
2026-03-06 19:15:58 +0530

    Create main.py

---
+++ api/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from api.schemas import Transaction, PredictionResponse, BatchRequest
+from api.model_service import FraudModelService
+
+app = FastAPI(
+    title="Fraud Detection API",
+    version="1.0",
+    description="Real-time credit card fraud detection using CatBoost"
+)
+
+model_service = FraudModelService()
+
+@app.get('/health')
+def health():
+    return {'status' : 'running'}
+
+@app.post('/predict', response_model = PredictionResponse)
+def predict(tnx: Transaction):
+    data = tnx.model_dump()
+    probability, prediction, risk = model_service.predict(data)
+    return {
+        "fraud_probability": probability,
+        "prediction": prediction,
+        "risk_level": risk
+    }
+
+@app.post(/predict-batch', response_model = PredictionResponse)
+def predict_batch(batch: BatchRequest)
+    results = []
+
+    for tx in batch.transactions:
+
+        data = tx.model_dump()
+
+        probability, prediction, risk = model_service.predict(data)
+
+        results.append({
+            "fraud_probability": probability,
+            "prediction": prediction,
+            "risk_level": risk
+        })
+
+    return {"predictions": results}
