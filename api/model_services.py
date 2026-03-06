9b93f612cca22e034cb6bd2104a8dae9653b3cd6
Abhi Raju Mallapu <49338480+mallapuabhiraj@users.noreply.github.com>
2026-03-06 18:38:33 +0530

    Update model_services.py

--- api/model_services.py
+++ api/model_services.py
@@ -1,5 +1,16 @@
-from catboost import CatBoostClassifier
-Class FraudModelService:
-    model = CatBoostClassifier()
-    model.load('models/cb_model')
-    preprocess_pipe = joblib.load('models/preprocess_pipe.joblib')
+from catboost import CatBoostClassifier, Pool
+import pandas as pd
+import joblib
+from api.config import FRAUD_THRESHOLD, CAT_COLS
+
+class FraudModelService:
+    def __init__(self):
+        self.model = CatBoostClassifier()
+        self.model.load_model('models/cb_model.cbm')
+        self.preprocess_pipe = joblib.load('models/preprocess_pipe.joblib')
+    def predict(self, data: dict):
+        df = pd.DataFrame([data])
+        df_preprocessed = self.preprocess_pipe.transform(df)
+        pool = Pool(df_preprocessed, cat_features=CAT_COLS)
+        proba = self.model.predict_proba(df_preprocessed)
+        pred = proba > FRAUD_THRESHOLD
