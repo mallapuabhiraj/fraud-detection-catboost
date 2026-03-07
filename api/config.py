8d71a643fa7a48e0eb752fb8be7ad3800e831082
Abhi Raju Mallapu <49338480+mallapuabhiraj@users.noreply.github.com>
2026-03-07 10:47:12 +0530

    Update config.py

--- api/config.py
+++ api/config.py
@@ -1,4 +1,4 @@
 MODEL_PATH = 'models/cb_model.cbm'
 PIPELINE_PATH = 'models/preprocess_pipe.joblib'
-FRAUD_THRESHOLD = 0.862
+FRAUD_THRESHOLD = 0.83
 CAT_COLS = ['age_group', 'category', 'state', 'job']
